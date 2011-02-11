b87866a621e9f5749dfcfc9c7051c04a8c5cbe31
tisue <tisue@37d2e27a-c460-0410-be0a-b6062e5d1e8b>
2011-02-11 18:15:24 +0000

    *** empty log message ***

--- src/90.scala
+++ src/90.scala
@@ -6,7 +6,7 @@ import Euler._
 class Problem90 extends Problem(90, "1217") {
   def solve = {
      val squares = List("01", "04", "09", "16", "25", "36", "49", "64", "81")
-    def isSolution(die1: List[Int], die2: List[Int]) = {
+    def isSolution(die1: Seq[Int], die2: Seq[Int]) = {
       val rolls = 
         for(d1 <- die1; d2 <- die2)
         yield d1.toString + d2.toString
@@ -16,7 +16,7 @@ class Problem90 extends Problem(90, "1217") {
       squares.forall(s => allRolls.contains(s) ||
                           allRolls.contains(s.reverse.mkString))
     }
-    val dies = choose(6, (0 to 9).toList)
+    val dies = (0 to 9).combinations(6).toSeq
     val pairs = for(die1 <- dies; die2 <- dies)
                 yield (die1, die2)
     pairs.count((isSolution _).tupled) / 2
