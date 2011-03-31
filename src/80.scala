642989230cbd6213ee36d48a40cd128789478410
tisue <tisue@37d2e27a-c460-0410-be0a-b6062e5d1e8b>
2011-03-31 17:27:22 +0000

    cleanup

--- src/80.scala
+++ src/80.scala
@@ -8,20 +8,26 @@ import Euler._
 // slow for this problem so I used this which is much simpler anyway:
 // http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
 
+import java.math.{RoundingMode => J}
+import scala.math.BigDecimal.{RoundingMode => S}
+
 class Problem80 extends Problem(80, "40886") {
   val digits = 100
   def isSquare(n: Int) = { val r = math.sqrt(n).toInt; r * r == n }
   // drop down to java.math.BigDecimal to use API not exposed by the Scala wrapper
   def roundingDivide(d1: BigDecimal, d2: BigDecimal) =
-    new BigDecimal(d1.bigDecimal.divide(d2.bigDecimal, digits, java.math.RoundingMode.DOWN))
+    new BigDecimal(d1.bigDecimal.divide(d2.bigDecimal, digits, J.DOWN))
   def digitalSum(d: BigDecimal) =
     d.toString.view.filter(_.isDigit).take(digits).map(_.asDigit).sum
   def firstDuplicate[A](xs: Seq[A]) =
     xs.zip(xs.tail).find{case (d1, d2) => d1 == d2}.get._1
   def babylonian(n: BigDecimal) =
-    firstDuplicate(
-      Stream.iterate(n)(x => ((x + roundingDivide(n, x)) / 2)
-        .setScale(digits, BigDecimal.RoundingMode.DOWN)))
+    Stream.iterate(n){next =>
+      ((next + roundingDivide(n, next)) / 2)
+        .setScale(digits, S.DOWN)}
   def solve =
-    (1 to 100).filter(!isSquare(_)).map(n => digitalSum(babylonian(n))).sum
+    (1 to 100)
+      .filter(!isSquare(_))
+      .map(n => digitalSum(firstDuplicate(babylonian(n))))
+      .sum
 }
