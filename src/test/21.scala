146bcb512481605acb6f4b749f4ae6403d90c1ae
Seth Tisue <seth@tisue.net>
2011-09-20 13:45:56 -0400

    reformat

--- src/test/21.scala
+++ src/test/21.scala
@@ -1,17 +1,14 @@
 package net.tisue.euler
 
-// Evaluate the sum of all the amicable numbers under 10000.
-// (In a pair of amicable numbers, each number is equal to the sum
-// of the other's divisors including 1.)
+// Evaluate the sum of all the amicable numbers under 10000.  (In a pair of amicable numbers, each
+// number is equal to the sum of the other's divisors including 1.)
 
-// Note: There is a slight ambiguity in the problem statement; it
-// doesn't say whether if a pair exists with one number below 10000
-// and one above, the lower number should be included in a sum.  As it
-// happens there is no such pair so it doesn't matter in this case,
-// but for a different bound besides 10000 it might matter.  Anyway,
-// it seems reasonable to interpret the problem such that both numbers
-// must be below the bound, and under that interpretation the code
-// below is correct in general.
+// Note: There is a slight ambiguity in the problem statement; it doesn't say whether if a pair
+// exists with one number below 10000 and one above, the lower number should be included in a sum.
+// As it happens there is no such pair so it doesn't matter in this case, but for a different bound
+// besides 10000 it might matter.  Anyway, it seems reasonable to interpret the problem such that
+// both numbers must be below the bound, and under that interpretation the code below is correct in
+// general.
 
 class Problem21 extends Problem(21, "31626") {
   val divisorSums =
