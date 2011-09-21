a085e239dfe6ff11b6d563082744c6e7f52301fe
Seth Tisue <seth@tisue.net>
2011-09-21 11:13:58 -0400

    reformat

--- src/test/87.scala
+++ src/test/87.scala
@@ -5,16 +5,14 @@ import Primes._
 // prime fourth power?  (There are exactly four such numbers below fifty.)
 
 class Problem87 extends Problem(87, "1097343") {
-  def solve = {
-    def solve(limit:Int) = {
-      val powers2 = primes.map(n => n * n        ).takeWhile(_ < limit)
-      val powers3 = primes.map(n => n * n * n    ).takeWhile(_ < limit)
-      val powers4 = primes.map(n => n * n * n * n).takeWhile(_ < limit)
-      (for{p4 <- powers4
-           p3 <- powers3
-           p2 <- powers2.takeWhile(_ <= limit - p4 - p3)}
+  def solve(limit: Int) = {
+    val powers2 = primes.map(n => n * n        ).takeWhile(_ < limit)
+    val powers3 = primes.map(n => n * n * n    ).takeWhile(_ < limit)
+    val powers4 = primes.map(n => n * n * n * n).takeWhile(_ < limit)
+    (for{p4 <- powers4
+         p3 <- powers3
+         p2 <- powers2.takeWhile(_ <= limit - p4 - p3)}
        yield p4 + p3 + p2).toSet.size
-    }
-    solve(50000000)
   }
+  def solve = solve(50000000)
 }
