245d884efeaafe898afb48f1b7b457f025ca1289
Seth Tisue <seth@tisue.net>
2011-09-27 23:16:48 -0400

    reformat

--- src/test/22.scala
+++ src/test/22.scala
@@ -3,10 +3,14 @@ package net.tisue.euler
 // What is the total of all the name scores in the file?
 
 class Problem22 extends Problem(22, "871198282") {
-  def solve = {
-    val names = io.Source.fromFile("dat/22.txt").filter(_ != '"').mkString.split(",").toList
-    def score(name: String) = name.map(_ - 'A' + 1).sum
-    (for ((name, index) <- names.sorted.zipWithIndex)
-     yield score(name) * (index + 1)).sum
-  }
+  val names =
+    io.Source.fromFile("dat/22.txt")
+      .filter(_ != '"').mkString
+      .split(",").toList
+  def score(name: String) =
+    name.map(_ - 'A' + 1).sum
+  def scores =
+    for ((name, index) <- names.sorted.zipWithIndex)
+    yield score(name) * (index + 1)
+  def solve = scores.sum
 }
