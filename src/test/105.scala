24391a5ea4de9ac754c1dd0d5602c9480a3b62e2
Seth Tisue <seth@tisue.net>
2011-11-15 16:29:44 -0500

    Problem 105: solved!

---
+++ src/test/105.scala
@@ -0,0 +1,27 @@
+package net.tisue.euler
+
+// Identify the special sum sets in sets.txt and find the sum of their sums.
+
+class Problem105 extends Problem(105, "73702") {
+  type SumSet = List[List[Int]]
+  def augment(ss: SumSet, x: Int) =
+    if(ss.isEmpty)
+      List(List(x))
+    else
+      (ss.head :+ x) +: ss.sliding(2).collect{case List(l1, l2) => l2 ++ l1.map(_ + x)}.toList :+ List(ss.head.sum + x)
+  def isSpecial(ss: SumSet): Boolean =
+    ss.size < 2 || ss.sliding(2).forall{
+      case List(xs1, xs2) =>
+        xs1.size == xs1.distinct.size &&
+        xs2.size == xs2.distinct.size &&
+        xs1.last < xs2.head
+      case _ => throw new IllegalStateException
+    }
+  def solve =
+    io.Source.fromFile("dat/105.txt").getLines
+      .map(_.split(",").map(_.toInt).sorted)
+      .map(_.foldLeft(Nil: SumSet)(augment))
+      .filter(isSpecial)
+      .map(_.head.sum)
+      .sum
+}
