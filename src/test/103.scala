9f68c39830139bcaa8ac612110d40fbbcbbc71dd
Seth Tisue <seth@tisue.net>
2011-11-15 15:51:10 -0500

    Problem 103: speedup

--- src/test/103.scala
+++ src/test/103.scala
@@ -11,30 +11,35 @@ class Problem103 extends Problem(103, "111819202225") {
     else
       (ss.head :+ x) +: ss.sliding(2).collect{case List(l1, l2) => l2 ++ l1.map(_ + x)}.toList :+ List(ss.head.sum + x)
   def isSpecial(ss: SumSet): Boolean =
-    ss.forall(xs => xs.size == xs.toSet.size) &&
-    ss.sliding(2).forall{case List(xs1, xs2) => xs1.size == xs1.distinct.size && xs2.size == xs2.distinct.size && xs1.last < xs2.head; case _ => true}
+    ss.size < 2 || ss.sliding(2).forall{
+      case List(xs1, xs2) =>
+        xs1.size == xs1.distinct.size &&
+        xs2.size == xs2.distinct.size &&
+        xs1.last < xs2.head
+      case _ => throw new IllegalStateException
+    }
   def solve = {
     val solutions =
       for {
         n1 <- Iterator.from(1)
         ss1 = augment(n1, Nil)
         if isSpecial(ss1)
-        n2 <- Iterator.from(n1 + 1).takeWhile(_ + ss1.head.sum <= ceil)
+        n2 <- Iterator.from(n1 + 1).takeWhile(_ + n1 <= ceil)
         ss2 = augment(n2, ss1)
         if isSpecial(ss2)
-        n3 <- Iterator.from(n2 + 1).takeWhile(_ + ss2.head.sum <= ceil)
+        n3 <- Iterator.from(n2 + 1).takeWhile(_ + n2 + n1 <= ceil)
         ss3 = augment(n3, ss2)
         if isSpecial(ss3)
-        n4 <- Iterator.from(n3 + 1).takeWhile(_ + ss3.head.sum <= ceil)
+        n4 <- Iterator.from(n3 + 1).takeWhile(_ + n3 + n2 + n1 <= ceil)
         ss4 = augment(n4, ss3)
         if isSpecial(ss4)
-        n5 <- Iterator.from(n4 + 1).takeWhile(_ + ss4.head.sum <= ceil)
+        n5 <- Iterator.from(n4 + 1).takeWhile(_ + n4 + n3 + n2 + n1 <= ceil)
         ss5 = augment(n5, ss4)
         if isSpecial(ss5)
-        n6 <- Iterator.from(n5 + 1).takeWhile(_ + ss5.head.sum <= ceil)
+        n6 <- Iterator.from(n5 + 1).takeWhile(_ + n5 + n4 + n3 + n2 + n1 <= ceil)
         ss6 = augment(n6, ss5)
         if isSpecial(ss6)
-        // n7 <- Iterator.from(n6 + 1).takeWhile(_ + ss6.head.sum <= ceil)
+        // n7 <- Iterator.from(n6 + 1).takeWhile(_ + n6 + n5 + n4 + n3 + n2 + n1 <= ceil)
         // ss7 = augment(n7, ss6)
         // if isSpecial(ss7)
       } yield ss6.head
