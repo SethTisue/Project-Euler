b3bcd5361f845d0f707fb7162e2148e1305d7d02
Seth Tisue <seth@tisue.net>
2012-11-21 15:52:51 -0500

    reformat

--- src/test/17.scala
+++ src/test/17.scala
@@ -4,14 +4,20 @@ package net.tisue.euler
 
 class Problem17 extends Problem(17, "21124") {
   def english(n: Int): String = {
-    if(n == 1000) "onethousand"
-    else if(n % 100 == 0) english(n / 100) + "hundred"
-    else if(n > 100) english(100 * (n / 100)) + "and" + english(n % 100)
-    else if(n < 20) List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-                         "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
-                         "seventeen", "eighteen", "nineteen")(n - 1)
-    else if(n % 10 == 0) List("twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")(n / 10 - 2)
-    else english(10 * (n / 10)) + "" + english(n % 10)
+    if(n == 1000)
+      "onethousand"
+    else if(n % 100 == 0)
+      english(n / 100) + "hundred"
+    else if(n > 100)
+      english(100 * (n / 100)) + "and" + english(n % 100)
+    else if(n < 20)
+      List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+        "seventeen", "eighteen", "nineteen")(n - 1)
+    else if(n % 10 == 0)
+      List("twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")(n / 10 - 2)
+    else
+      english(10 * (n / 10)) + "" + english(n % 10)
   }
   def solve = (1 to 1000).map(english(_).size).sum
 }
