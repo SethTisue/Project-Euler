0eb37b3073ffabb7e9d0631aec90b28e0e815819
Seth Tisue <seth@tisue.net>
2013-07-29 20:44:21 -0400

    solve Problem 59 without using a dictionary file

--- src/test/59.scala
+++ src/test/59.scala
@@ -8,24 +8,31 @@ package net.tisue.euler
 // sum of the ASCII values in the original text.
 
 class Problem59 extends Problem(59, "107359") {
-  // Using an actual English dictionary is arguably overkill; we could use heuristics instead.
-  // But using a dictionary is easy enough, and runs fast enough.
+  // extremely rough heuristic, but it's good enough in this case
+  def isEnglish(s: String): Boolean =
+    (s.split(' ').size / s.size.toDouble) > 0.15
+  def decrypt(s: String, key: Seq[Char]) =
+    s.toStream.zip(key.toStream.circular)
+     .map{case (c1, c2) => c1 ^ c2}.map(_.toChar).mkString
+  val cipherText =
+    io.Source.fromFile("dat/59.txt").mkString.trim.split(",").map(_.toInt.toChar).mkString
+  val allKeys =
+    for{c1 <- 'a' to 'z'; c2 <- 'a' to 'z'; c3 <- 'a' to 'z'}
+    yield List(c1, c2, c3)
+  val key =
+    allKeys.filter(key => isEnglish(decrypt(cipherText, key)))
+      .ensuring(_.size == 1)
+      .head
+  def solve =
+    decrypt(cipherText, key).map(_.toInt).sum
+}
+
+/*
+a less janky way to write isEnglish, assuming you have a dictionary available:
   val dict = io.Source.fromFile("/usr/share/dict/words")("ISO-8859-1")
                .getLines.map(_.trim.toLowerCase).toSet
   def isEnglish(s: String): Boolean = {
     val words = s.toLowerCase.filter((c:Char) => c == ' ' || c.isLetter).mkString.split(' ')
     0.5 < words.count(dict.contains(_)) / words.size.toDouble
   }
-  def decrypt(s: String, key: Seq[Char]) =
-    s.toStream.zip(key.toStream.circular)
-     .map{case (c1, c2) => c1 ^ c2}.map(_.toChar).mkString
-  val cipherText =
-    io.Source.fromFile("dat/59.txt").mkString.trim.split(",").map(_.toInt.toChar).mkString
-  val key =
-    (for{c1 <- 'a' to 'z'; c2 <- 'a' to 'z'; c3 <- 'a' to 'z'}
-     yield List(c1, c2, c3))
-    .find(key => isEnglish(decrypt(cipherText, key)))
-    .get
-  def solve =
-    decrypt(cipherText, key).map(_.toInt).sum
-}
+*/
