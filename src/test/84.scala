60d71191a58546e5e5b90d77d602cec9873b29f3
Seth Tisue <seth@tisue.net>
2015-04-19 18:54:54 -0400

    reformatting & minor code improvement

--- src/test/84.scala
+++ src/test/84.scala
@@ -1,30 +1,37 @@
 package net.tisue.euler
 
-// In a game of Monopoly, statistically it can be shown that the three most popular squares, in
-// descending order, are JAIL, E3, and GO (squares 10, 24, 0 = 102400). If, instead of using two
-// 6-sided dice, two 4-sided dice are used, find the three winners.
-
-// This can be solved by Monte Carlo simulation, but I chose to approach it analytically as a
-// Markov chain problem:
-//   www.math.yorku.ca/Who/Faculty/Steprans/Courses/2042/Monopoly/Stewart2.html
-//   www.tkcs-collins.com/truman/monopoly/monopoly.shtml
-//   en.wikipedia.org/wiki/Markov_matrix
-
-// The rule about rolling doubles three times in a row doesn't affect the probabilities enough to
-// change the final answer, so we just ignore that rule.  Someone in the forum points out that
-// "If you are on CH3 and go back on CC3, shouldn't you take another card?" but that doesn't
-// affect the final answer either.
+// In a game of Monopoly, statistically it can be shown that the three most
+// popular squares, in descending order, are JAIL, E3, and GO (squares 10, 24, 0
+// = 102400). If, instead of using two 6-sided dice, two 4-sided dice are used,
+// find the three winners.
+//
+// This can be solved by Monte Carlo simulation, but I chose to approach it
+// analytically as a Markov chain problem:
+//   http://www.math.yorku.ca/Who/Faculty/Steprans/Courses/2042/Monopoly/Stewart2.html
+//   http://www.tkcs-collins.com/truman/monopoly/monopoly.shtml
+//   http://en.wikipedia.org/wiki/Markov_matrix
+//
+// The rule about rolling doubles three times doesn't affect the probabilities
+// enough to change the final answer, so we just ignore that rule.
+//
+// Someone in the forum points out that "If you are on CH3 and go back on CC3,
+// shouldn't you take another card?" but that doesn't affect the final answer
+// either.
 
 class Problem84 extends Problem(84, solution = "101524") {
+
   val die = 4
   val names = List("GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3",
                    "JAIL", "C1", "U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3",
                    "FP", "E1", "CH2", "E2", "E3", "R3", "F1", "F2", "U2", "F3",
                    "G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2")
-  val namesCycle = names.toStream.circular
   val squares = names.zipWithIndex.toMap  // map from name to number
-  def limit(squareNumber: Int) = (squareNumber + names.size) % names.size
-  def nextSquare(square: Int, roll: Int) = {
+  val namesCycle = names.toStream.circular
+
+  def limit(squareNumber: Int): Int =
+    (squareNumber + names.size) % names.size
+
+  def nextSquare(square: Int, roll: Int): List[BigRational] = {
     def nextSpecial(square: Int, kind: Char) =  // kind is R for railroad or U for utility
       limit(square + namesCycle.drop(square).indexWhere(_.head == kind))
     val newSquare = (square + roll) % names.size
@@ -39,28 +46,38 @@ class Problem84 extends Problem(84, solution = "101524") {
                limit(square - 3)) ::: List.fill(6)(newSquare)
         case _ => List(newSquare)
       }
-    names.indices.toList.map(next => new BigRational(nexts.count(_ == next),
-                                                     nexts.size * die * die))
+    names.indices.toList.map(next =>
+      new BigRational(
+        nexts.count(_ == next),
+        nexts.size * die * die))
   }
+
   // P is the Markov matrix
   val P: List[List[Double]] = {
     val zeroVector = List.fill(names.size)(0: BigRational)
-    val rolls = for(die1 <- 1 to die; die2 <- 1 to die)
-                yield die1 + die2
+    val rolls =
+      for(die1 <- 1 to die; die2 <- 1 to die)
+      yield die1 + die2
     def row(i: Int) =
-      rolls.toList.foldLeft(zeroVector)(
-        (vec, roll) => (vec, nextSquare(i, roll)).zipped.map(_ + _))
+      rolls.toList.foldLeft(zeroVector){(vec, roll) =>
+        (vec, nextSquare(i, roll)).zipped.map(_ + _)}
     names.indices.toList.map(row(_).map(_.toDouble))
   }
-  def matrixMul(m1: List[List[Double]], m2: List[List[Double]]) =
-    m1.indices.map(i =>
-      m1.indices.map(j =>
-        m1.indices.map(r => m1(i)(r) * m2(r)(j)).sum).toList).toList
+
+  def matrixMul(m1: List[List[Double]], m2: List[List[Double]]) = {
+    val indices = m1.indices.toList
+    indices.map(i =>
+      indices.map(j =>
+        indices.map(r => m1(i)(r) * m2(r)(j))
+          .sum))
+  }
+
   // as k approaches infinity, all rows of P^k approach the stationary probability vector.
   // trial and error says k=150 gives accuracy to at least 3 decimal places
   def solve = {
     val stationaryProbabilityVector =
-      Iterator.iterate(P)(matrixMul(_, P)).drop(150).next.head
+      Iterator.iterate(P)(matrixMul(_, P))
+        .drop(150).next.head
     stationaryProbabilityVector
       .zipWithIndex
       .sortBy(-_._1)
@@ -69,4 +86,5 @@ class Problem84 extends Problem(84, solution = "101524") {
       .map("%02d".format(_))
       .mkString
   }
+
 }
