c5fcded438288080c44c5aca8a1c7a9ec3c5a35f
Seth Tisue <seth@tisue.net>
2015-12-07 20:15:11 -0800

    get tests running in pants build
    and have Travis run Pants instead of sbt

--- build.sbt
+++ build.sbt
@@ -3,6 +3,9 @@ scalaVersion := "2.11.7"
 name := "Seth's Project Euler solutions"
 
 libraryDependencies +=
+  "junit" % "junit" % "4.12" % "test"
+
+libraryDependencies +=
   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 scalaSource in Compile := baseDirectory.value / "src" / "main"
--- src/test/net/tisue/euler/Problem.scala
+++ src/test/net/tisue/euler/Problem.scala
@@ -1,7 +1,10 @@
 package net.tisue.euler
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 
+@RunWith(classOf[JUnitRunner])
 abstract class Problem(number: Int, solution: String) extends FunSuite {
   test(s"problem $number") {
     val (result, elapsed) = time(solve)
