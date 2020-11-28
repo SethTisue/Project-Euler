7298dc3baf6963834fdb24b0e545c3ab807ffe07
Seth Tisue <seth@tisue.net>
2020-11-28 11:06:40 -0800

    oh this locally isn't needed either

--- src/test/scala/net/tisue/euler/68.scala
+++ src/test/scala/net/tisue/euler/68.scala
@@ -8,7 +8,7 @@ class Problem68 extends Problem(68, "6531031914842725"):
     ns(0) < ns(3) &&
       ns(0) < ns(5) &&
       ns(0) < ns(7) &&
-      ns(0) < ns(9) && locally:
+      ns(0) < ns(9) && :
         val sum = ns(0) + ns(1) + ns(2)
         sum == ns(3) + ns(2) + ns(4) &&
           sum == ns(5) + ns(4) + ns(6) &&
