5d119e602f00afc0bfe3dd5efebb18858d0fce47
Seth Tisue <seth@tisue.net>
2023-08-10 09:54:19 -0700

    arguable code improvement

--- src/test/scala/net/tisue/euler/102.scala
+++ src/test/scala/net/tisue/euler/102.scala
@@ -8,16 +8,21 @@ package net.tisue.euler
 // out the math.
 
 class Problem102 extends Problem(102, "228"):
-  def check(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double) =
-    val q = (((x2 - x3) * y2) - ((y2 - y3) * x2)) /
-      (((y3 - y2) * x1) - ((x3 - x2) * y1))
+  case class Point(x: Double, y: Double)
+  case class Triangle(p1: Point, p2: Point, p3: Point)
+  def check(t: Triangle): Boolean =
+    import t.*
+    val q = (((p2.x - p3.x) * p2.y) - ((p2.y - p3.y) * p2.x)) /
+      (((p3.y - p2.y) * p1.x) - ((p3.x - p2.x) * p1.y))
     q < 0 || q > 1
   def solve =
     io.Source.fromResource("102.txt").getLines
-      .map(_.trim.split(",").toList.map(_.toInt))
       .collect:
-        case Seq(x1, y1, x2, y2, x3, y3) =>
-          check(x1, y1, x2, y2, x3, y3) &&
-            check(x2, y2, x1, y1, x3, y3) &&
-            check(x3, y3, x1, y1, x2, y2)
+        case s"$x1,$y1,$x2,$y2,$x3,$y3" =>
+          val p1 = Point(x1.toDouble, y1.toDouble)
+          val p2 = Point(x2.toDouble, y2.toDouble)
+          val p3 = Point(x3.toDouble, y3.toDouble)
+          check(Triangle(p1, p2, p3)) &&
+            check(Triangle(p2, p1, p3)) &&
+            check(Triangle(p3, p1, p2))
       .count(identity)
