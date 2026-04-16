6b53bad14594c7386eadaf9fc752cea642e405fd
Seth Tisue <seth@tisue.net>
2026-04-16 08:19:10 -0700

    fix up java options in build

--- project.scala
+++ project.scala
@@ -2,3 +2,4 @@
 //> using test.dep org.scalameta::munit:1.3.0
 //> using options -encoding us-ascii -deprecation -feature -Werror -source:future -Wunused:all
 //> using resourceDir inputs
+//> using javaOpt -Xmx6G -XX:MaxInlineLevel=18
