d46f596b3e84951302ffcd807f80786980e408f3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-11 21:02:55 +0200

    Update munit to 1.3.4 (#165)

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
 //> using scala 3.8.4
-//> using test.dep org.scalameta::munit:1.3.3
+//> using test.dep org.scalameta::munit:1.3.4
 //> using options -encoding us-ascii -deprecation -feature -Werror -source:future -Wunused:all
 //> using resourceDir inputs
 //> using javaOpt -Xmx6G -XX:MaxInlineLevel=18
