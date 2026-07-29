444cafeb06f2d3332bae29a613806d4f29cca619
Seth Tisue <seth@tisue.net>
2026-07-29 14:56:41 -0700

    Scala 3.9.0-RC4 (was 3.8.4)

--- project.scala
+++ project.scala
@@ -1,4 +1,4 @@
-//> using scala 3.8.4
+//> using scala 3.9.0-RC4
 //> using test.dep org.scalameta::munit:1.3.4
 //> using options -encoding us-ascii -deprecation -feature -Werror -source:future -Wunused:all
 //> using resourceDir inputs
